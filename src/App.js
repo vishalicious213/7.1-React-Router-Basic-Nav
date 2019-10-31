2ca0498121683f60d5f751ba17eea41620762d99
vishalicious213 <vishalicious213@gmail.com>
2019-10-31 01:12:33 -0400

    Imported Link into Navigation.js. Set up Link to routes. MVP

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-<Route exact path='/' component={Home} />
-<Route path='/about' component={About} />
-<Route path='/contact' component={Contact} />
+
 
 const App = () => (
   <div>
     <Navigation />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
   </div>
 );
 
